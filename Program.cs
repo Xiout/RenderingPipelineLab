f43830670de0e04b8e07146e5322d9a749ad946e
pdxAlexiaDauvergne <alexia.dauvergne@paradoxinteractive.com>
2024-12-22 18:46:51 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RenderingPipelineLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
