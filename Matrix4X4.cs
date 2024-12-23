5c1ad4ac5b587d3eb297b895ac3a3111579ba65f
pdxAlexiaDauvergne <alexia.dauvergne@gmail.com>
2024-12-23 14:05:54 +0100

    Impl. Matrix4X4 with Add and Multiply + UnitTests

---
+++ Matrix4X4.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderingPipelineLab
+{
+    public class Matrix4X4
+    {
+        private float[] _coord;
+
+        public Matrix4X4(float[] row0, float[] row1, float[] row2, float[] row3)
+        {
+            if (row0 == null || row0.Length != 4) { throw new ArgumentException($"Parameter row0 should has a lenght of 4 but was {(row0 == null ? "null" : row0.Length.ToString())}"); }
+            if (row1 == null || row1.Length != 4) { throw new ArgumentException($"Parameter row1 should has a lenght of 4 but was {(row1 == null ? "null" : row1.Length.ToString())}"); }
+            if (row2 == null || row2.Length != 4) { throw new ArgumentException($"Parameter row2 should has a lenght of 4 but was {(row2 == null ? "null" : row2.Length.ToString())}"); }
+            if (row3 == null || row3.Length != 4) { throw new ArgumentException($"Parameter row3 should has a lenght of 4 but was {(row3 == null ? "null" : row3.Length.ToString())}"); }
+
+            List<float> coord = new List<float>();
+            coord.AddRange(row0);
+            coord.AddRange(row1);
+            coord.AddRange(row2);
+            coord.AddRange(row3);
+
+            _coord = coord.ToArray();
+        }
+
+        public Matrix4X4(float[] coord) 
+        { 
+            if(coord == null || coord.Length != 16) { throw new ArgumentException($"Parameter coord should has a lenght of 16 but was {(coord == null ? "null" : coord.Length.ToString())}"); }
+            _coord = coord;
+        }
+
+        public float GetElement(int row, int column)
+        {
+            if(row < 0 || row >= 4)
+            {
+                throw new ArgumentException($"row parameter {row} was out of bound");
+            }
+
+            if (column < 0 || column >= 4)
+            {
+                throw new ArgumentException($"column parameter {column} was out of bound");
+            }
+
+
+            return _coord[row*4+column];
+        }
+
+        public static Matrix4X4 Add(Matrix4X4 MatA, Matrix4X4 MatB)
+        {
+            float[] coord = new float[16];
+
+            for(int i_row = 0; i_row < 4; ++i_row)
+            {
+                for (int i_col = 0; i_col < 4; ++i_col)
+                {
+                    coord[i_row*4+i_col] = MatA.GetElement(i_row, i_col) + MatB.GetElement(i_row, i_col);
+                }
+            }
+
+            return new Matrix4X4(coord);
+        }
+
+        public static Matrix4X4 Multiply(Matrix4X4 MatA, Matrix4X4 MatB)
+        {
+            float[] coord = new float[16];
+
+            for(int row = 0; row < 4; ++row)
+            {
+                for (int col = 0; col < 4; ++col)
+                {
+                    float element = 0.0f;
+                    for(int i = 0; i < 4; ++i)
+                    {
+                        element += (float)MatA.GetElement(row, i) * (float)MatB.GetElement(i, col);
+                    }
+                    coord[row*4+col] = element;
+                }
+            }
+
+            return new Matrix4X4(coord);
+        }
+    }
+}
---
+++ RenderingPipelineLab.Tests/Matrix4X4Tests.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenderingPipelineLab.Tests
+{
+    [TestFixture]
+    public class Matrix4X4Tests
+    {
+        [TestCase(
+            new float[] { 1.0f, -2.0f, 3.0f, 4.5f }, new float[] { 2.0f, 3.0f, -1.0f, 0.0f }, new float[] { 5.0f, -1.0f, 7.0f, 1.0f }, new float[] { 0.0f, 0.0f, -4.0f, 8.0f },
+            new float[] { -1.0f, 2.0f, -3.0f, 4.0f }, new float[] { 4.0f, -2.0f, 1.0f, 0.0f }, new float[] { -5.0f, 0.0f, 2.0f, -1.0f }, new float[] { 3.0f, 1.0f, 0.0f, -3.0f },
+            new float[] { 0.0f, 0.0f, 0.0f, 8.5f }, new float[] { 6.0f, 1.0f, 0.0f, 0.0f }, new float[] { 0.0f, -1.0f, 9.0f, 0.0f }, new float[] { 3.0f, 1.0f, -4.0f, 5.0f }
+        )]
+        public void Test_Add(float[] MatARow0,float[] MatARow1, float[] MatARow2, float[] MatARow3,
+                             float[] MatBRow0, float[] MatBRow1, float[] MatBRow2, float[] MatBRow3,
+                             float[] expRow0 , float[] expRow1 , float[] expRow2 , float[] expRow3)
+        {
+            // Arrange
+            Matrix4X4 matA = new Matrix4X4(MatARow0, MatARow1, MatARow2, MatARow3);
+            Matrix4X4 matB = new Matrix4X4(MatBRow0, MatBRow1, MatBRow2, MatBRow3);
+            Matrix4X4 expectedResult = new Matrix4X4(expRow0, expRow1, expRow2, expRow3);
+
+            // Act
+            Matrix4X4 result = Matrix4X4.Add(matA, matB);
+
+            // Assert
+            StringBuilder sb = new StringBuilder();
+            bool isEqual = true;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (result.GetElement(i, j) != expectedResult.GetElement(i, j))
+                    {
+                        sb.Append($"row {i} column {j}, expected: {expectedResult.GetElement(i, j)} but was: {result.GetElement(i,j)}\n");
+                        isEqual = false;
+                    }
+                }
+            }
+
+            Assert.IsTrue( isEqual , sb.ToString());
+        }
+
+        [TestCase(
+            new float[] { 2.0f, 1.0f, 1.0f, 0.0f }, new float[] { 0.0f, 3.0f, 1.0f, 0.0f }, new float[] { 1.0f, 0.0f, 2.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 1.0f },
+            new float[] { 3.0f, 2.0f, 1.0f, 0.0f }, new float[] { 1.0f, 2.0f, 3.0f, 0.0f }, new float[] { 2.0f, 1.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 1.0f },
+            new float[] { 9.0f, 7.0f, 5.0f, 0.0f }, new float[] { 5.0f, 7.0f, 9.0f, 0.0f }, new float[] { 7.0f, 4.0f, 1.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 1.0f }
+            )]
+        [TestCase(
+            new float[] { 1.0f, -2.0f, 3.0f, 4.5f }, new float[] { 2.0f, 3.0f, -1.0f, 0.0f }, new float[] { 5.0f, -1.0f, 7.0f, 1.0f }, new float[] { 0.0f, 0.0f, -4.0f, 8.0f },
+            new float[] { -1.0f, 2.0f, -3.0f, 4.0f }, new float[] { 4.0f, -2.0f, 1.0f, 0.0f }, new float[] { -5.0f, 0.0f, 2.0f, -1.0f }, new float[] { 3.0f, 1.0f, 0.0f, -3.0f },
+            new float[] { -10.5f, 10.5f, 1.0f, -12.5f }, new float[] { 15.0f, -2.0f, -5.0f, 9.0f }, new float[] { -41.0f, 13.0f, -2.0f, 10.0f }, new float[] { 44.0f, 8.0f, -8.0f, -20.0f }
+        )]
+        [TestCase(
+            new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f },
+            new float[] { 1.0f, 2.0f, 3.0f, 4.0f }, new float[] { 5.0f, 6.0f, 7.0f, 8.0f }, new float[] { 9.0f, 10.0f, 11.0f, 12.0f }, new float[] { 13.0f, 14.0f, 15.0f, 16.0f },
+            new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }
+        )]//With Zero Matrix
+        [TestCase(
+            new float[] { 1.0f, 2.0f, 3.0f, 4.0f }, new float[] { 5.0f, 6.0f, 7.0f, 8.0f }, new float[] { 9.0f, 10.0f, 11.0f, 12.0f }, new float[] { 13.0f, 14.0f, 15.0f, 16.0f },
+            new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f },
+            new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f }
+        )]//With Zero Matrix
+        [TestCase(
+            new float[] { 1.0f, 2.0f, 3.0f, 4.0f }, new float[] { 5.0f, 6.0f, 7.0f, 8.0f }, new float[] { 9.0f, 10.0f, 11.0f, 12.0f }, new float[] { 13.0f, 14.0f, 15.0f, 16.0f },
+            new float[] { 1.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 1.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 1.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 1.0f },
+            new float[] { 1.0f, 2.0f, 3.0f, 4.0f }, new float[] { 5.0f, 6.0f, 7.0f, 8.0f }, new float[] { 9.0f, 10.0f, 11.0f, 12.0f }, new float[] { 13.0f, 14.0f, 15.0f, 16.0f }
+        )]//Identity
+
+        public void Test_Multiply(float[] MatARow0, float[] MatARow1, float[] MatARow2, float[] MatARow3,
+                                  float[] MatBRow0, float[] MatBRow1, float[] MatBRow2, float[] MatBRow3,
+                                  float[] expRow0 , float[] expRow1 , float[] expRow2 , float[] expRow3)
+        {
+            // Arrange
+            Matrix4X4 matA = new Matrix4X4(MatARow0, MatARow1, MatARow2, MatARow3);
+            Matrix4X4 matB = new Matrix4X4(MatBRow0, MatBRow1, MatBRow2, MatBRow3);
+            Matrix4X4 expectedResult = new Matrix4X4(expRow0, expRow1, expRow2, expRow3);
+
+            // Act
+            Matrix4X4 result = Matrix4X4.Multiply(matA, matB);
+
+            // Assert
+            StringBuilder sb = new StringBuilder();
+            bool isEqual = true;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (result.GetElement(i, j) != expectedResult.GetElement(i, j))
+                    {
+                        sb.Append($"row {i} column {j}, expected: {expectedResult.GetElement(i, j)} but was: {result.GetElement(i, j)}\n");
+                        isEqual = false;
+                    }
+                }
+            }
+
+            Assert.IsTrue(isEqual, sb.ToString());
+        }
+    }
+}
